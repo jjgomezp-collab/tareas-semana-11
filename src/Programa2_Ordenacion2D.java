c3734ae9c9b407611b157d9d2dd3d98de9d30bd8
jjgomezp-colaboración <jj.gomezp@uea.edu.ec>
2025-08-29 20:06:05 -0600

    Corrección en Programa2: eliminado código duplicado

--- src/Programa2_Ordenacion2D.java
+++ src/Programa2_Ordenacion2D.java
@@ -28,15 +28,3 @@ public class Programa2_Ordenacion2D {
         imprimirMatriz(matriz);
     }
 }
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Admin
- */
-public class Programa2_Ordenacion2D {
-    
-}
